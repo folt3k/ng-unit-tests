96ff3dd45cbf9761dc186cbe4c2f15d347ccb5a9
folt3k <folt3k@gmail.com>
2023-03-28 23:07:25 +0200

    async testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { FormsModule } from "@angular/forms";
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/blog/blog.component.spec.ts
@@ -0,0 +1,69 @@
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+  waitForAsync,
+} from '@angular/core/testing';
+
+import { BlogComponent } from './blog.component';
+import { BlogService } from '../../services/blog.service';
+import { delay, of } from 'rxjs';
+
+describe('BlogComponent', () => {
+  let component: BlogComponent;
+  let fixture: ComponentFixture<BlogComponent>;
+  let blogServiceSpy: jasmine.SpyObj<BlogService>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BlogComponent],
+      providers: [
+        {
+          provide: BlogService,
+          useValue: jasmine.createSpyObj('BlogService', ['getArticle$']),
+        },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BlogComponent);
+    component = fixture.componentInstance;
+    blogServiceSpy = TestBed.inject(BlogService) as jasmine.SpyObj<BlogService>;
+    fixture.detectChanges();
+  });
+
+  it('should render article title - sync test', () => {
+    blogServiceSpy.getArticle$.and.returnValue(of({ title: 'foo' }));
+
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.textContent).toMatch(/foo/i);
+  });
+
+  it('should render article title - fakeAsync test', fakeAsync(() => {
+    blogServiceSpy.getArticle$.and.returnValue(
+      of({ title: 'foo' }).pipe(delay(1000))
+    );
+
+    fixture.detectChanges();
+
+    tick(1000);
+
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.textContent).toMatch(/foo/i);
+  }));
+
+  it('should render article title - waitForAsync test', waitForAsync(() => {
+    blogServiceSpy.getArticle$.and.returnValue(
+      of({ title: 'foo' }).pipe(delay(1000))
+    );
+
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(fixture.nativeElement.textContent).toMatch(/foo/i);
+    });
+  }));
+});
